284bdb508c514e3e09dc963a2f840ff14e43d249
alexgit55 <121723219+alexgit55@users.noreply.github.com>
2026-09-02 15:43:43 -0400

    Add more unit tests and configure CircleCI pipeline

--- test_calculator.py
+++ test_calculator.py
@@ -5,6 +5,13 @@ class TestCalculator:
 
     def test_addition(self):
         assert calculator.add(1, 2) == 3
+        assert calculator.add(-3, 5) == 2
+        assert calculator.add(0, 0) == (
+            0
+        )
 
     def test_subtraction(self):
-        assert calculator.subtract(5, 3) == 2
+        assert calculator.subtract(10, 5) == 5
+        assert calculator.subtract(-3, 5) == -8
+        assert calculator.subtract(0, 0) == 0
+
